ec1db05b1f074d22654022622f85edb1a16fa369
alexmanrique <alexmanrique@users.noreply.github.com>
2026-06-20 09:49:50 +0000

    deploy: 83fbe70853453a8b64f66192ef81d245291d7882

---
+++ assets/arrow-right-yfLpshwv.js
@@ -0,0 +1,8 @@
+import{w as o}from"./core-Dfkqo3ex.js";import"./index-7TS2r0U1.js";import"./index.es-CCPY7b4L.js";import"./events-DQ172AOg.js";import"./index-nibyPLVP.js";const p=o`<svg fill="none" viewBox="0 0 14 15">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M1 7.99a1 1 0 0 1 1-1h7.58L7.12 4.53A1 1 0 1 1 8.54 3.1l4.16 4.17a1 1 0 0 1 0 1.41l-4.16 4.17a1 1 0 1 1-1.42-1.41l2.46-2.46H2a1 1 0 0 1-1-1Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{p as arrowRightSvg};
